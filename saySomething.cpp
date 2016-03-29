ca83e8f12ea9c75af919c6d63be4c647c15792d6
bgeri74 <bgeri74@gmail.com>
2016-03-29 17:21:54 +0200

    Added comments and made the code a bit cleaner.

--- saySomething.cpp
+++ saySomething.cpp
@@ -1,8 +1,12 @@
 #include "eSpeak.h"
 #include <iostream>
 
+/* Program to say something. */
+
 int main(int argc, char* argv[]){
  
+  // We use the function saySomething to tell the computer what to say and how to say it. We provide it with the second argument which is the sentence to be said.
   saySomething(argv[1]);
+  
   return 0;
 }
